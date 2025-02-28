d00ae859b29546ab9fe206b1186cf608230699fd
Chelsy Octaviani <143569397+ChelsyOctaviani@users.noreply.github.com>
2025-02-28 12:21:52 +0700

    index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// Menentukan halaman yang akan ditampilkan berdasarkan parameter GET
 $halaman = isset($_GET['halaman']) ? $_GET['halaman'] : 'todo';
 ?>
 
@@ -15,40 +16,40 @@ $halaman = isset($_GET['halaman']) ? $_GET['halaman'] : 'todo';
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
         crossorigin="anonymous">
 
-    <!-- Font Awesome CSS -->
+    <!-- Font Awesome CSS untuk ikon -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 
-<!-- content -->
 <body>
- <div class="container-fluid">
-    <div class="row d-flex justify-content-center mt-3 vh-100">
-        <div class="col-md-12 bg-info-subtle p-4">
-            <?php
-            if ($halaman == 'todo') {
-                include 'todo/todo.php';
-            } elseif ($halaman == 'edit_todo') {
-                include 'todo/edit_todo.php';
-            }
-            ?>
+    <!-- Container utama -->
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center mt-3 vh-100">
+            <div class="col-md-12 bg-info-subtle p-4">
+                <?php
+                // Menampilkan halaman berdasarkan nilai variabel $halaman
+                if ($halaman == 'todo') {
+                    include 'todo/todo.php'; // Memuat halaman daftar todo
+                } elseif ($halaman == 'edit_todo') {
+                    include 'todo/edit_todo.php'; // Memuat halaman edit todo
+                }
+                ?>
+            </div>
         </div>
     </div>
- </div>
-<!-- content -->
 
-<!-- Bootstrap JS -->
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
- integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
- crossorigin="anonymous"></script>
+    <!-- Bootstrap JS untuk komponen interaktif -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 
- <!-- Jquery -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- jQuery untuk manipulasi elemen DOM -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-  <script>
-    // Auto close alert
-    $(".alert").delay(1000).slideUp(200, function () {
-        $(this).alert('close');
-    });
+    <script>
+        // Auto close alert setelah 1 detik
+        $(".alert").delay(1000).slideUp(200, function () {
+            $(this).alert('close');
+        });
     </script>
 </body>
 
